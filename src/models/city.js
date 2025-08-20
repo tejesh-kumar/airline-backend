f896917d450309e9c06c09cbd69f86582d292003
tejesh-kumar <tejeshmuralikrishna@gmail.com>
2025-08-20 18:55:25 +0530

    associations represent table relations at ORM level for one-many relationship (city can have multiple airports) using cityId

--- src/models/city.js
+++ src/models/city.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.hasMany(models.Airport, { foreignKey: 'cityId' })
     }
   }
   City.init(
