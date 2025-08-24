4e8217dd6610d4ae378c53e1d91865dd333b59a9
tejesh-kumar <tejeshmuralikrishna@gmail.com>
2025-08-24 13:38:18 +0530

    validation check for "Departure time cannot be earlier than arrival time" for incoming flight payload

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,11 +1,19 @@
 const { StatusCodes } = require('http-status-codes')
 const { FlightRepository } = require('../repositories')
 const AppError = require('../utils/errors/app-error')
+const { compareTime } = require('../utils/helpers/datetime-helpers')
 
 const flightRepository = new FlightRepository()
 
 async function createFlight(data) {
   try {
+    const isDepartureBeforeArrival = compareTime(
+      data?.arrivalTime,
+      data?.departureTime
+    )
+    if (!isDepartureBeforeArrival) {
+      throw new Error('departureBeforeArrivalError')
+    }
     const flight = await flightRepository.create(data)
     return flight
   } catch (error) {
@@ -16,7 +24,10 @@ async function createFlight(data) {
       })
       throw new AppError(explanation, StatusCodes.BAD_REQUEST)
     }
-    console.log(error)
+    if (error.message === 'departureBeforeArrivalError') {
+      let explanation = 'Departure time cannot be earlier than arrival time'
+      throw new AppError(explanation, StatusCodes.BAD_REQUEST)
+    }
     throw new AppError(
       'Cannot create a new flight obj',
       StatusCodes.INTERNAL_SERVER_ERROR
